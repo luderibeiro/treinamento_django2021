eb596af63f05706e6eb93ed487ae5131e0203e7b
Lude Ribeiro <ludeyuridrums@gmail.com>
2021-12-03 12:57:25 -0300

    subindo dia 3

--- blog/polls/views.py
+++ blog/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Alô mundo doido")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, "polls/index.html", {'latest_question_list': latest_question_list, 'titulo': 'Lista de Questões',})
